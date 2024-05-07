4a7718f21affa4cb6ebe74bcbff6623630636c52
Konrad Ostrowski <konradostrrowski@gmail.com>
2024-05-07 13:58:35 +0200

    added tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class ModelTest(SimpleTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.post = Post.objects.create(text="another boring test")
+
+    def test_model(self):
+        self.assertEqual(self.post.text, "another boring test")
+
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "another boring test")
